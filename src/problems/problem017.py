5f1c8978b5b34c34d96411432fbd9e0d800321f6
Niko Wilbert <niko.wilbert@tngtech.com>
2011-05-12 17:08:44 +0200

    Some refactoring.

--- src/problems/problem017.py
+++ src/problems/problem017.py
@@ -5,8 +5,7 @@ zero_to_twenty = ["zero", "one", "two", "three", "four", "five", "six", "seven",
                  "nineteen"]
 ten_to_hundred= [None, "ten", "twenty", "thirty", "forty", "fifty", "sixty",
                  "seventy", "eighty", "ninety"]
-two_digit_compounds = [None, ]
-baseten_postfixes = [None, None, None, "hundred", "thousand"]
+baseten = [None, None, None, "hundred", "thousand"]
 
 def textual_number(n):
     """Return the textual form of the given number.
@@ -23,37 +22,37 @@ def textual_number(n):
         >>> textual_number(90)
         'ninety'
         >>> textual_number(100)
-        'one hundred'
+        'one-hundred'
         >>> textual_number(199)
-        'one hundred and ninety-nine'
+        'one-hundred and ninety-nine'
         >>> textual_number(411)
-        'four hundred and eleven'
+        'four-hundred and eleven'
         >>> textual_number(1000)
-        'one thousand'
+        'one-thousand'
     """
     digits = [int(d) for d in str(n)]
-    last_two = digits[-1]
-    if len(digits) > 1:
-        last_two += 10 * digits[-2]
-    # deal with first two digits
-    if n == 0:
-        s = zero_to_twenty[0]
-    elif last_two == 0:
-        s = ""
-    elif 0 < last_two < 20:
-        s = zero_to_twenty[last_two]
-    else:
-        s = ten_to_hundred[digits[-2]]
-        if digits[-1]:
-            s += "-" + zero_to_twenty[digits[-1]]
+    s = ""
+    # fourth digit
+    if n == 1000:
+        s += zero_to_twenty[1] + "-" + baseten[4]
     # deal with third digit
     if n >= 100 and digits[-3]:
         if s:
-            s = " and " + s
-        s = zero_to_twenty[digits[-3]] + " " + baseten_postfixes[3] + s
-    # fourth digit
-    if n == 1000:
-        s = zero_to_twenty[1] + " " + baseten_postfixes[4]
+            s += " and "
+        s += zero_to_twenty[digits[-3]] + "-" + baseten[3]
+    # deal with first two digits
+    last_two = n % 100
+    if s and last_two:
+        s += " and "
+    elif not s and not last_two:
+        s += zero_to_twenty[0]
+    if last_two:
+        if 0 < last_two < 20:
+            s += zero_to_twenty[last_two]
+        else:
+            s += ten_to_hundred[digits[-2]]
+            if digits[-1]:
+                s += "-" + zero_to_twenty[digits[-1]]
     return s
 
 if __name__ == "__main__":
